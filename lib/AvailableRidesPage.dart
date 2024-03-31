811a707096b94b3328a7e42032e4b00bf36797bf
pankhis <psaini2@ncsu.edu>
2024-03-31 02:28:59 -0400

    Available rides fixed

--- lib/AvailableRidesPage.dart
+++ lib/AvailableRidesPage.dart
@@ -1,5 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
+class AvailableRidesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Available Rides'),
+        centerTitle: true,
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('rides').snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+          if (snapshot.hasData && snapshot.data!.docs.isEmpty) {
+            return Center(child: Text('No available rides'));
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              var ride = snapshot.data!.docs[index];
+              return Card(
+                margin: EdgeInsets.all(10.0),
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          // Text(
+                          //   ride['name'],
+                          //   style: TextStyle(fontWeight: FontWeight.bold),
+                          // ),
+                          Text(ride['destination']),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(DateFormat('yyyy-MM-dd').format(ride['date'].toDate())),
+                          Text(DateFormat('HH:mm').format(ride['time'].toDate())),
+                        ],
+                      ),
+                      SizedBox(height: 8.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text('Seats: ${ride['seats']}'),
+                          Text('Cost: \$${ride['amount']}'),
+                        ],
+                      ),
+                      SizedBox(height: 16.0),
+                      ElevatedButton(
+                        onPressed: () {
+                          // Implement request ride functionality
+                        },
+                        child: Text('Book a seat'),
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Color(0xFFF930204),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -11,68 +95,6 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: AvailableRidesPage(),
-    );
-  }
-}
-
-class AvailableRidesPage extends StatelessWidget {
-  final List<Map<String, dynamic>> rides = [
-    {'name': 'Name', 'destination': 'Destination', 'date': 'Date', 'time': 'Time'},
-    // Add more ride details here
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Available Rides'),
-        centerTitle: true,
-      ),
-      body: ListView.builder(
-        itemCount: rides.length,
-        itemBuilder: (context, index) {
-          var ride = rides[index];
-          return Card(
-            margin: EdgeInsets.all(10.0),
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        ride['name'],
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      Text(ride['destination']),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(ride['date']),
-                      Text(ride['time']),
-                    ],
-                  ),
-                  SizedBox(height: 16.0),
-                  ElevatedButton(
-                    onPressed: () {
-                      // Implement request ride functionality
-                    },
-                    child: Text('Request'),
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.white, 
-                      backgroundColor: Color(0xFFF930204),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
     );
   }
 }
