ed33dd6e6a6186d3a08418b9671068ca338c136c
pankhis <psaini2@ncsu.edu>
2024-03-31 04:41:28 -0400

    Home Page fixed

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,8 +1,95 @@
+// import 'package:flutter/material.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
+// import './RideOptionScreen.dart';
+
+
+// class HomePage extends StatelessWidget {
+  
+//   @override
+//   Widget build(BuildContext context) {
+//     Future<void> _signInWithGoogle() async {
+//   try {
+//     // Initialize GoogleSignIn with your client ID
+//     final GoogleSignIn googleSignIn = GoogleSignIn(clientId: '128602331951-rkdhgh1r7s348d3uindqclkv2b3rims2.apps.googleusercontent.com');
+    
+//     final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+//     final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+//     final credential = GoogleAuthProvider.credential(
+//       accessToken: googleAuth?.accessToken,
+//       idToken: googleAuth?.idToken,
+//     );
+//     await FirebaseAuth.instance.signInWithCredential(credential);
+//     Navigator.push(
+//       context,
+//       MaterialPageRoute(builder: (context) => RideOptionScreen()),
+//     );
+//   } catch (error) {
+//     print(error);
+//   }
+// }
+//     return Scaffold(
+//       body: Padding(
+//         padding: EdgeInsets.all(16.0),
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Text('Log in to your account', style: TextStyle(fontSize: 24.0)),
+//             SizedBox(height: 20),
+//             ElevatedButton(
+//               onPressed: _signInWithGoogle,
+//               child: Text('Continue with Google'),
+//               style: ElevatedButton.styleFrom(
+//                 foregroundColor: Colors.black, 
+//                 backgroundColor: Colors.white,
+//                 minimumSize: Size(double.infinity, 50),
+//               ),
+//             ),
+//             SizedBox(height: 20),
+//             Row(children: <Widget>[
+//               Expanded(child: Divider()),
+//               Padding(
+//                 padding: const EdgeInsets.symmetric(horizontal: 8.0),
+//                 child: Text('OR'),
+//               ),
+//               Expanded(child: Divider()),
+//             ]),
+//             SizedBox(height: 20),
+//             TextFormField(
+//               decoration: InputDecoration(
+//                 labelText: 'Email',
+//                 border: OutlineInputBorder(),
+//               ),
+//             ),
+//             SizedBox(height: 20),
+//             TextFormField(
+//               obscureText: true,
+//               decoration: InputDecoration(
+//                 labelText: 'Password',
+//                 border: OutlineInputBorder(),
+//               ),
+//             ),
+//             SizedBox(height: 20),
+//             ElevatedButton(
+//               onPressed: () {},
+//               child: Text('Log in'),
+//               style: ElevatedButton.styleFrom(
+//                 backgroundColor: Color(0xFFF930204),
+//                 minimumSize: Size(double.infinity, 50),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import './RideOptionScreen.dart';
-
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class HomePage extends StatelessWidget {
   
@@ -29,56 +116,26 @@ class HomePage extends StatelessWidget {
   }
 }
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Log in to your account', style: TextStyle(fontSize: 24.0)),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _signInWithGoogle,
-              child: Text('Continue with Google'),
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.black, 
-                backgroundColor: Colors.white,
-                minimumSize: Size(double.infinity, 50),
+      body: Center( // Center the content vertically and horizontally
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Log in to your account', style: TextStyle(fontSize: 24.0)),
+              SizedBox(height: 40),
+              SignInButton(
+                Buttons.Google,
+                text: "Continue with Google",
+                onPressed: _signInWithGoogle,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(4)),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12), // Increase button padding
+                elevation: 0, // Remove button elevation
               ),
-            ),
-            SizedBox(height: 20),
-            Row(children: <Widget>[
-              Expanded(child: Divider()),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: Text('OR'),
-              ),
-              Expanded(child: Divider()),
-            ]),
-            SizedBox(height: 20),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 20),
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Log in'),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFFF930204),
-                minimumSize: Size(double.infinity, 50),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/RideOptionScreen.dart
+++ lib/RideOptionScreen.dart
@@ -1,33 +1,127 @@
+// import 'package:flutter/material.dart';
+// import 'package:pooling/DestinationSearch.dart';
+// import './postride.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+
+
+
+// class RideOptionScreen extends StatelessWidget {
+//   // late User _currentUser;
+
+//   // @override
+//   // void initState() {
+//   //   // Get the current user when the widget initializes
+//   //   super.initState();
+//   //   _currentUser = FirebaseAuth.instance.currentUser!;
+//   // }
+
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Choose Your Option'),
+//       ),
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         crossAxisAlignment: CrossAxisAlignment.stretch,
+//         children: <Widget>[
+//           // Text(
+//           //     _currentUser.displayName ?? 'User',
+//           //     style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+//           //   ),
+//           Padding(
+//             padding: const EdgeInsets.all(20.0),
+//             child: Text(
+//               'How can we help you today?',
+//               style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+//               textAlign: TextAlign.center,
+//             ),
+//           ),
+//           OptionCard(
+//             icon: Icons.directions_car,
+//             text: 'Find me a ride!',
+//             onTap: () {
+//               Navigator.push(
+//                   context,
+//                 MaterialPageRoute(builder: (context) => DestinationSearch()),
+//     );
+//   },
+//           ),
+//           OptionCard(
+//             icon: Icons.note_add,
+//             text: 'Post my ride!',
+//              onTap: () {
+//               Navigator.push(
+//                   context,
+//                 MaterialPageRoute(builder: (context) => postride()),
+//     );
+//   },
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// class OptionCard extends StatelessWidget {
+//   final IconData icon;
+//   final String text;
+//   final VoidCallback onTap;
+
+//   OptionCard({required this.icon, required this.text, required this.onTap});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return InkWell(
+//       onTap: onTap,
+//       child: Card(
+//         margin: EdgeInsets.all(8.0),
+//         child: Container(
+//           padding: EdgeInsets.all(16.0),
+//           child: Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: <Widget>[
+//               Icon(icon, size: 40.0),
+//               SizedBox(width: 24.0),
+//               Text(
+//                 text,
+//                 style: TextStyle(fontSize: 18.0),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:pooling/DestinationSearch.dart';
 import './postride.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-
-
 class RideOptionScreen extends StatelessWidget {
-  // late User _currentUser;
-
-  // @override
-  // void initState() {
-  //   // Get the current user when the widget initializes
-  //   super.initState();
-  //   _currentUser = FirebaseAuth.instance.currentUser!;
-  // }
-
+  @override
   Widget build(BuildContext context) {
+    // Get the current user
+    User? currentUser = FirebaseAuth.instance.currentUser;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Choose Your Option'),
+        title: Text('Logged In Successfully'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          // Text(
-          //     _currentUser.displayName ?? 'User',
-          //     style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          //   ),
+          currentUser != null ? // Check if currentUser is not null
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text(
+              'Welcome, ${currentUser.displayName}!', // Display the user's display name
+              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+          ) : Container(), // If currentUser is null, display an empty Container
           Padding(
             padding: const EdgeInsets.all(20.0),
             child: Text(
@@ -37,24 +131,24 @@ class RideOptionScreen extends StatelessWidget {
             ),
           ),
           OptionCard(
-            icon: Icons.directions_car,
+            imagePath: 'images/findme.png', // Path to the image asset
             text: 'Find me a ride!',
             onTap: () {
               Navigator.push(
-                  context,
+                context,
                 MaterialPageRoute(builder: (context) => DestinationSearch()),
-    );
-  },
+              );
+            },
           ),
           OptionCard(
-            icon: Icons.note_add,
+            imagePath: 'images/postnew.png', // Path to the image asset
             text: 'Post my ride!',
-             onTap: () {
+            onTap: () {
               Navigator.push(
-                  context,
+                context,
                 MaterialPageRoute(builder: (context) => postride()),
-    );
-  },
+              );
+            },
           ),
         ],
       ),
@@ -63,11 +157,11 @@ class RideOptionScreen extends StatelessWidget {
 }
 
 class OptionCard extends StatelessWidget {
-  final IconData icon;
+  final String imagePath;
   final String text;
   final VoidCallback onTap;
 
-  OptionCard({required this.icon, required this.text, required this.onTap});
+  OptionCard({required this.imagePath, required this.text, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
@@ -77,14 +171,19 @@ class OptionCard extends StatelessWidget {
         margin: EdgeInsets.all(8.0),
         child: Container(
           padding: EdgeInsets.all(16.0),
-          child: Row(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Icon(icon, size: 40.0),
-              SizedBox(width: 24.0),
+              SizedBox(
+                height: 120.0, // Adjust the height of the image container
+                width: 120.0, // Adjust the width of the image container
+                child: Image.asset(imagePath, fit: BoxFit.cover), // Use BoxFit.cover to scale the image
+              ),
+              SizedBox(height: 16.0), // Add some space between the image and text
               Text(
                 text,
                 style: TextStyle(fontSize: 18.0),
+                textAlign: TextAlign.center,
               ),
             ],
           ),
